5cab0b10e8255468179de57b919dd95d47e043d6
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2022-10-27 13:13:39 +0200

    SQMAGOPC-444: validate customer on changing card

--- Controller/Card/Change.php
+++ Controller/Card/Change.php
@@ -11,9 +11,11 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Action;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Validation\ValidationException;
 use Magento\Vault\Model\ResourceModel\PaymentToken;
 use Paytrail\PaymentService\Api\Data\SubscriptionInterface;
 use Paytrail\PaymentService\Api\SubscriptionRepositoryInterface;
+use Paytrail\PaymentService\Model\Validation\PreventAdminActions;
 use Psr\Log\LoggerInterface;
 
 class Change extends Action\Action
@@ -39,24 +41,32 @@ class Change extends Action\Action
     private PaymentToken $paymentToken;
 
     /**
+     * @var PreventAdminActions
+     */
+    private PreventAdminActions $preventAdminActions;
+
+    /**
      * @param Context $context
      * @param SubscriptionRepositoryInterface $subscriptionRepositoryInterface
      * @param LoggerInterface $logger
      * @param PaymentToken $paymentToken
      * @param Session $customerSession
+     * @param PreventAdminActions $preventAdminActions
      */
     public function __construct(
         Context                             $context,
         SubscriptionRepositoryInterface     $subscriptionRepositoryInterface,
         LoggerInterface                     $logger,
         PaymentToken                        $paymentToken,
-        Session                             $customerSession
+        Session                             $customerSession,
+        PreventAdminActions $preventAdminActions
     ) {
         parent::__construct($context);
         $this->subscriptionRepositoryInterface = $subscriptionRepositoryInterface;
         $this->logger = $logger;
         $this->paymentToken = $paymentToken;
         $this->customerSession = $customerSession;
+        $this->preventAdminActions = $preventAdminActions;
     }
 
     /**
@@ -65,6 +75,10 @@ class Change extends Action\Action
      */
     public function execute()
     {
+        if ($this->preventAdminActions->isAdminAsCustomer()) {
+            throw new ValidationException(__('Admin user is not authorized for this operation'));
+        }
+
         $subscriptionId = $this->getRequest()->getParam('subscription_id');
         $selectedTokenRaw = $this->getRequest()->getParam('selected_token');
 
