21800d8763ce18554dc886c55d8fb1bc5e5848a9
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2022-11-17 15:28:16 +0100

    SQMAGOPC-446: add proxy into constructor

--- Model/Company/CompanyCommerceRequestData.php
+++ Model/Company/CompanyCommerceRequestData.php
@@ -2,9 +2,8 @@
 
 namespace Paytrail\PaymentService\Model\Company;
 
-use Magento\Company\Api\CompanyRepositoryInterface;
+use Magento\Company\Api\CompanyRepositoryInterface\Proxy as CompanyRepositoryProxy;
 use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Customer\Model\Session;
 
 /**
  * Class CompanyCommerceRequestData
@@ -12,34 +11,22 @@ use Magento\Customer\Model\Session;
 class CompanyCommerceRequestData
 {
     /**
-     * @var Session
-     */
-    private Session $customerSession;
-
-    /**
      * @var CustomerRepositoryInterface
      */
     private CustomerRepositoryInterface $customerRepository;
+    private CompanyRepositoryProxy $companyRepositoryProxy;
 
     /**
-     * @var CompanyRepositoryInterface
-     */
-    private CompanyRepositoryInterface $companyRepository;
-
-    /**
-     * @param Session $customerSession
      * @param CustomerRepositoryInterface $customerRepository
-     * @param CompanyRepositoryInterface $companyRepository
+     * @param CompanyRepositoryProxy $companyRepositoryProxy
      */
     public function __construct(
-        Session $customerSession,
         CustomerRepositoryInterface $customerRepository,
-        CompanyRepositoryInterface $companyRepository
+        CompanyRepositoryProxy $companyRepositoryProxy
     )
     {
-        $this->customerSession = $customerSession;
         $this->customerRepository = $customerRepository;
-        $this->companyRepository = $companyRepository;
+        $this->companyRepositoryProxy = $companyRepositoryProxy;
     }
 
     /**
@@ -50,12 +37,15 @@ class CompanyCommerceRequestData
      */
     public function setCompanyCommerceRequestData($customer)
     {
-        $companyId = $this->customerRepository->get($customer->getEmail())->getExtensionAttributes()->getCompanyAttributes()->getCompanyId() ?? null;
-            if ($companyId) {
-                $customer->setCompanyName($this->companyRepository->get($companyId)->getCompanyName());
+        $companyId = $this->customerRepository->get($customer->getEmail())
+            ->getExtensionAttributes()
+            ->getCompanyAttributes()
+            ->getCompanyId();
+        if ($companyId) {
+            $customer->setCompanyName($this->companyRepositoryProxy->get($companyId)->getCompanyName());
 
-                return $customer;
-            }
+            return $customer;
+        }
 
         return $customer;
     }
