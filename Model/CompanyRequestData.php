f6481f9bfcde4e45de23c5d106294942bcab9732
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2022-11-17 12:44:17 +0100

    SQMAGOPC-446: Add validations

--- Model/CompanyRequestData.php
+++ Model/CompanyRequestData.php
@@ -2,6 +2,7 @@
 
 namespace Paytrail\PaymentService\Model;
 
+use Magento\Company\Api\CompanyRepositoryInterface;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\Session;
 
@@ -14,21 +15,49 @@ class CompanyRequestData
      * @var Session
      */
     private Session $customerSession;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
     private CustomerRepositoryInterface $customerRepository;
 
+    /**
+     * @var CompanyRepositoryInterface
+     */
+    private CompanyRepositoryInterface $companyRepository;
+
+    /**
+     * @param Session $customerSession
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param CompanyRepositoryInterface $companyRepository
+     */
     public function __construct(
         Session $customerSession,
         CustomerRepositoryInterface $customerRepository,
+        CompanyRepositoryInterface $companyRepository
     )
     {
         $this->customerSession = $customerSession;
         $this->customerRepository = $customerRepository;
+        $this->companyRepository = $companyRepository;
     }
 
     public function setCompanyRequestData($customer, $billingAddress)
     {
-        if (!$customer->getCompanyName() && !$this->customerSession->isLoggedIn()) {
+        if ($billingAddress->getCompany()) {
             $customer->setCompanyName($billingAddress->getCompany());
+
+            return $customer;
+        }
+        if ($this->customerSession->isLoggedIn()) {
+            $companyId = $this->customerRepository->get($customer->getEmail())->getExtensionAttributes()->getCompanyAttributes()->getCompanyId() ?? null;
+            if ($companyId) {
+                $customer->setCompanyName($this->companyRepository->get($companyId)->getCompanyName());
+
+                return $customer;
+            }
+
+            return $customer;
         }
 
         return $customer;
