15e912b4d775396a5af010f695dca897af0ad4fa
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2022-11-18 12:35:29 +0100

    SQMAGOPC-446: remove code for company_extensions

--- Model/Company/CompanyRequestData.php
+++ Model/Company/CompanyRequestData.php
@@ -2,51 +2,15 @@
 
 namespace Paytrail\PaymentService\Model\Company;
 
-use Magento\Customer\Model\Session;
-use Magento\Framework\Module\Manager;
-use Paytrail\PaymentService\Model\Company\CompanyCommerceRequestData\Proxy as CompanyCommerceRequestDataProxy;
-
 /**
  * Class CompanyRequestData
  */
 class CompanyRequestData
 {
     /**
-     * @var Session
-     */
-    private $customerSession;
-
-    /**
-     * @var Manager
-     */
-    private Manager $moduleManager;
-
-    /**
-     * @var CompanyCommerceRequestDataProxy
-     */
-    private CompanyCommerceRequestDataProxy $companyCommerceRequestData;
-
-    /**
-     * @param Session $customerSession
-     * @param Manager $moduleManager
-     * @param CompanyCommerceRequestDataProxy   $companyCommerceRequestData
-     */
-    public function __construct(
-        Session                    $customerSession,
-        Manager                    $moduleManager,
-        CompanyCommerceRequestDataProxy $companyCommerceRequestData
-    ) {
-        $this->customerSession = $customerSession;
-        $this->moduleManager = $moduleManager;
-        $this->companyCommerceRequestData = $companyCommerceRequestData;
-    }
-
-    /**
      * @param $customer
      * @param $billingAddress
      * @return mixed
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function setCompanyRequestData($customer, $billingAddress)
     {
@@ -54,9 +18,6 @@ class CompanyRequestData
             $customer->setCompanyName($billingAddress->getCompany());
 
             return $customer;
-        }
-        if ($this->customerSession->isLoggedIn() && $this->moduleManager->isEnabled('Magento_Company')) {
-            $this->companyCommerceRequestData->setCompanyCommerceRequestData($customer);
         }
 
         return $customer;
