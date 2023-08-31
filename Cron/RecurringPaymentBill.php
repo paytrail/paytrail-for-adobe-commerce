7ac24ecbaf3d1ba5d8ad4890e6a7514cebdfa72e
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2023-08-31 09:54:06 +0200

    SQMAGOPC-515: disable cron jobs on config value

--- Cron/RecurringPaymentBill.php
+++ Cron/RecurringPaymentBill.php
@@ -2,19 +2,40 @@
 
 namespace Paytrail\PaymentService\Cron;
 
+use Paytrail\PaymentService\Model\Recurring\TotalConfigProvider;
+
 class RecurringPaymentBill
 {
     /** @var \Paytrail\PaymentService\Model\Recurring\Bill */
     private $bill;
 
+    /**
+     * @var TotalConfigProvider
+     */
+    private $totalConfigProvider;
+
+    /**
+     * @param \Paytrail\PaymentService\Model\Recurring\Bill $bill
+     * @param TotalConfigProvider $totalConfigProvider
+     */
     public function __construct(
-        \Paytrail\PaymentService\Model\Recurring\Bill $bill
+        \Paytrail\PaymentService\Model\Recurring\Bill $bill,
+        TotalConfigProvider $totalConfigProvider
     ) {
         $this->bill = $bill;
+        $this->totalConfigProvider = $totalConfigProvider;
     }
 
+    /**
+     * Execute.
+     *
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function execute()
     {
-        $this->bill->process();
+        if ($this->totalConfigProvider->isRecurringPaymentEnabled()) {
+            $this->bill->process();
+        }
     }
 }
