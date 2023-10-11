a2fcac49aa4696cfaf98d67f453f053fc167d849
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2023-10-11 17:58:40 +0200

    SQMAGOPC-455: improve code

--- Model/Invoice/Activation/Flag.php
+++ Model/Invoice/Activation/Flag.php
@@ -3,20 +3,20 @@ declare(strict_types=1);
 
 namespace Paytrail\PaymentService\Model\Invoice\Activation;
 
+use Paytrail\PaymentService\Gateway\Config\Config;
 use Paytrail\SDK\Request\PaymentRequest;
 
 class Flag
 {
-    private const ACTIVE_INVOICE_CONFIG_PATH = 'payment/paytrail/activate_invoices_separately';
     public const SUB_METHODS_WITH_MANUAL_ACTIVATION_SUPPORT = [
         'collectorb2c',
         'collectorb2b',
     ];
 
     /**
-     * @var \Paytrail\PaymentService\Gateway\Config\Config
+     * @var Config
      */
-    private \Paytrail\PaymentService\Gateway\Config\Config $config;
+    private Config $config;
 
     /**
      * Array of sub method codes similar to "collectorb2c", codes added to array will get manual invoicing flag
@@ -27,11 +27,11 @@ class Flag
     private array $activationOverride;
 
     /**
-     * @param \Paytrail\PaymentService\Gateway\Config\Config $config
+     * @param Config $config
      * @param array $activationOverride
      */
     public function __construct(
-        \Paytrail\PaymentService\Gateway\Config\Config $config,
+        Config $config,
         array $activationOverride = []
     ) {
         $this->config = $config;
