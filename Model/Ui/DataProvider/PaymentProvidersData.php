55d1094eaa769561ebaa214eb29de029b41a8b8e
konrad.konieczny <konrad.konieczny@solteq.com>
2023-11-06 19:08:26 +0100

    allow methods get for 0 total

--- Model/Ui/DataProvider/PaymentProvidersData.php
+++ Model/Ui/DataProvider/PaymentProvidersData.php
@@ -43,7 +43,7 @@ class PaymentProvidersData
      */
     public function getAllPaymentMethods()
     {
-        $orderValue = $this->checkoutSession->getQuote()->getGrandTotal();
+        $orderValue = $this->checkoutSession->getQuote()->getGrandTotal() ?: 0;
 
         $commandExecutor = $this->commandManagerPool->get('paytrail');
         $response = $commandExecutor->executeByCode(
@@ -92,7 +92,7 @@ class PaymentProvidersData
             . $this->gatewayConfig->getPaymentMethodHighlightColor($storeId) . ';border-width:2px;}';
         $styles .= '.paytrail-payment-methods .paytrail-stored-token.active{ border-color:'
             . $this->gatewayConfig->getPaymentMethodHighlightColor($storeId) . ';border-width:2px;}';
-        $styles .= '.paytrail-payment-methods .paytrail-payment-method:hover, 
+        $styles .= '.paytrail-payment-methods .paytrail-payment-method:hover,
         .paytrail-payment-methods .paytrail-payment-method:not(.active):hover { border-color:'
             . $this->gatewayConfig->getPaymentMethodHoverHighlight($storeId) . ';}';
         $styles .= $this->gatewayConfig->getAdditionalCss($storeId);
