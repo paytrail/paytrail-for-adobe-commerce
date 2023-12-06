e6364dbf4bfc6468024137e978920003cbc39255
konrad.konieczny <konrad.konieczny@solteq.com>
2023-12-06 17:58:39 +0100

    SQMAGOPC-542: fix callback controllers, add logging

--- Controller/Callback/Index.php
+++ Controller/Callback/Index.php
@@ -9,6 +9,7 @@ use Magento\Framework\Controller\ResultInterface;
 use Magento\Sales\Model\OrderFactory;
 use Paytrail\PaymentService\Controller\Receipt\Index as Receipt;
 use Paytrail\PaymentService\Gateway\Config\Config;
+use Paytrail\PaymentService\Logger\PaytrailLogger;
 use Paytrail\PaymentService\Model\Receipt\ProcessPayment;
 
 class Index implements \Magento\Framework\App\ActionInterface
@@ -24,12 +25,13 @@ class Index implements \Magento\Framework\App\ActionInterface
      * @param OrderFactory $orderFactory
      */
     public function __construct(
-        private Session $session,
-        private ProcessPayment $processPayment,
+        private Session          $session,
+        private ProcessPayment   $processPayment,
         private RequestInterface $request,
-        private ResultFactory $resultFactory,
-        private Config $gatewayConfig,
-        private OrderFactory $orderFactory
+        private ResultFactory    $resultFactory,
+        private Config           $gatewayConfig,
+        private OrderFactory     $orderFactory,
+        private PaytrailLogger   $logger
     ) {
     }
 
@@ -41,8 +43,9 @@ class Index implements \Magento\Framework\App\ActionInterface
      */
     public function execute(): ResultInterface
     {
-        $reference = $this->request->getParam('checkout-reference');
-        $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $reference     = $this->request->getParam('checkout-reference');
+        $response      = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $responseError = [];
 
         /** @var string $orderNo */
         $orderNo = $this->gatewayConfig->getGenerateReferenceForOrder()
@@ -50,19 +53,34 @@ class Index implements \Magento\Framework\App\ActionInterface
             : $reference;
 
         /** @var \Magento\Sales\Model\Order $order */
-        $order = $this->orderFactory->create()->loadByIncrementId($orderNo);
+        $order  = $this->orderFactory->create()->loadByIncrementId($orderNo);
         $status = $order->getStatus();
+
+        $this->logger->debugLog(
+            'request',
+            'callback received' . PHP_EOL .
+            'order status: ' . $status . PHP_EOL .
+            'orderNo: ' . $orderNo . PHP_EOL .
+            'params: ' . json_encode($this->request->getParams(), JSON_PRETTY_PRINT)
+        );
 
         if ($status == 'pending_payment' || in_array($status, Receipt::ORDER_CANCEL_STATUSES)) {
             // order status could be changed by receipt
             // if not, status change needs to be forced by processing the payment
-            return $response->setData(
-                [
-                    'error' => $this->processPayment->process($this->request->getParams(), $this->session)
-                ]
-            );
+
+            $responseError = $this->processPayment->process($this->request->getParams(), $this->session);
         }
 
-        return $response;
+        $responseData = ['error' => $responseError];
+
+        $this->logger->debugLog(
+            'request',
+            'callback response' . PHP_EOL .
+            'order status: ' . $status . PHP_EOL .
+            'orderNo: ' . $orderNo . PHP_EOL .
+            'response: ' . json_encode($responseData, JSON_PRETTY_PRINT)
+        );
+
+        return $response->setData($responseData);
     }
 }
