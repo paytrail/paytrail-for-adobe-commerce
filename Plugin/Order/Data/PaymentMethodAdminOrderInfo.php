ee9c3cc06d89bbd43c8bd567a0d95575d2f69daa
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-03-07 09:15:09 +0100

    SQMAGOPC-632: improve code

--- Plugin/Order/Data/PaymentMethodAdminOrderInfo.php
+++ Plugin/Order/Data/PaymentMethodAdminOrderInfo.php
@@ -16,16 +16,13 @@ class PaymentMethodAdminOrderInfo
      */
     public function aroundGetPaymentHtml(Info $subject)
     {
-        if ($subject->getOrder()->getPayment()->getMethod() === Config::CODE &&
-            isset($subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE])
-        ) {
+        if ($subject->getOrder()->getPayment()->getMethod() === Config::CODE) {
             return $subject->getChildHtml('order_payment')
                 . $subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::METHOD_TITLE_CODE]
+                    ->getAdditionalInformation(OrderPaymentMethodData::METHOD_TITLE_CODE)
                 . ' ('
                 . $subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE]
+                    ->getAdditionalInformation(OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE)
                 . ')';
         } else {
             return $subject->getChildHtml('order_payment');
--- Plugin/Order/Data/PaymentMethodCustomerOrderInfo.php
+++ Plugin/Order/Data/PaymentMethodCustomerOrderInfo.php
@@ -16,15 +16,12 @@ class PaymentMethodCustomerOrderInfo
      */
     public function aroundGetPaymentInfoHtml(Info $subject)
     {
-        if ($subject->getOrder()->getPayment()->getMethod() === Config::CODE &&
-            isset($subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE])
-        ) {
+        if ($subject->getOrder()->getPayment()->getMethod() === Config::CODE) {
             return $subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::METHOD_TITLE_CODE]
+                    ->getAdditionalInformation(OrderPaymentMethodData::METHOD_TITLE_CODE)
                 . ' ('
                 . $subject->getOrder()->getPayment()
-                    ->getAdditionalInformation()[OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE]
+                    ->getAdditionalInformation(OrderPaymentMethodData::SELECTED_PAYMENT_METHOD_CODE)
                 . ')';
         } else {
             return $subject->getChildHtml('payment_info');
