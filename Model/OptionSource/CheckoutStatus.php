5ac1e36ed7bca23df04d0f5e4ea1ea11f04d4bae
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-03-20 10:51:37 +0100

    SQMAGOPC-638: fix code-validations

--- Model/OptionSource/CheckoutStatus.php
+++ Model/OptionSource/CheckoutStatus.php
@@ -7,6 +7,8 @@ use Magento\Framework\Data\OptionSourceInterface;
 class CheckoutStatus implements OptionSourceInterface
 {
     /**
+     * Provide options for filter.
+     *
      * @return array[]
      */
     public function toOptionArray()
